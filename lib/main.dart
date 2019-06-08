d3e88a1cef4a83a64f5ceef9b3925b0e5bbf2827
Rajat Mangal <rajatmangal@Rajats-MBP.hitronhub.home>
2019-06-07 23:23:22 -0700

    initial setup completed

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,34 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(
-      MaterialApp(
-        home: null,
+void main() {
+  return runApp(
+    MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("Ask Me Anything"),
+          backgroundColor: Colors.blue.shade900,
+        ),
+        backgroundColor: Colors.blue,
+        body: EightBall(),
+      ),
+    ),
+  );
+}
+
+
+class EightBall extends StatefulWidget {
+  @override
+  _EightBallState createState() => _EightBallState();
+}
+
+class _EightBallState extends State<EightBall> {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Image(
+        image: AssetImage("images/ball1.png"),
       ),
     );
+  }
+}
+
